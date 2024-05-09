4dda1f3df3d6a2112c3dd336cc448f7e6464ea42
Felipe Rodrigues <65414548+lferodrigues@users.noreply.github.com>
2024-05-09 19:57:17 -0300

    Update scripts.js

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -42,8 +42,6 @@ function getCookie(name) {
     }
     return null;
 }
-</script>
-<script>
 // Modal de Imagens
 function onClick(element) {
   document.getElementById("img01").src = element.src;
@@ -67,4 +65,53 @@ function w3_open() {
 //Feche a barra lateral com o botão Fechar - Não utilizado na primeira versão
 function w3_close() {
     mySidebar.style.display = "none";
+}
+// Função que será executada quando a janela for completamente carregada
+window.onload = function() {
+    // Obtém o elemento onde será exibido o sistema operacional
+    var osSpan = document.getElementById('os');
+    
+    // Chama a função para detectar o sistema operacional e exibi-lo
+    var osName = detectOS();
+    osSpan.textContent = osName;
+
+    // Atualiza o ano no rodapé
+    document.getElementById('currentYear').textContent = new Date().getFullYear();
+};
+
+// Função para detectar o sistema operacional do usuário
+function detectOS() {
+    var userAgent = window.navigator.userAgent, // Obtém o user-agent do navegador
+        platform = window.navigator.platform,   // Obtém a plataforma do sistema operacional
+        macPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'], // Plataformas do MacOS
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],     // Plataformas do Windows
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],                     // Plataformas do iOS
+        os = null;
+
+    // Verifica se a plataforma corresponde a uma das plataformas MacOS
+    if (macPlatforms.indexOf(platform) !== -1) {
+        os = 'Mac OS';
+    } 
+    // Verifica se a plataforma corresponde a uma das plataformas iOS
+    else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = 'iOS';
+    } 
+    // Verifica se a plataforma corresponde a uma das plataformas Windows
+    else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'Windows';
+    } 
+    // Verifica se o user-agent contém a string "Android"
+    else if (/Android/.test(userAgent)) {
+        os = 'Android';
+    } 
+    // Verifica se a plataforma corresponde a Linux
+    else if (!os && /Linux/.test(platform)) {
+        os = 'Linux';
+    } 
+    // Se nenhuma das condições acima for satisfeita, assume-se que é outro sistema operacional
+    else {
+        os = 'Outro';
+    }
+
+    return os; // Retorna o nome do sistema operacional detectado
 }
